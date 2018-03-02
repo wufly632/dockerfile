12686527e9bbc332cfce14fe022ccd50c6b6de9a
wufly <wfxykzd@163.com>
2018-03-02 13:49:02 +0800

    wf

--- dnmp/www/index.php
+++ dnmp/www/index.php
@@ -2,6 +2,3 @@
 echo "<h1><center>hello wufly-dnmp</center></h1>";
 phpinfo();
 
-UPDATE pms_product_words_lib SET en_name = CONCAT(UPPER(SUBSTR(en_name,1,1)), SUBSTR(en_name,2,LENGTH(en_name)-1)) WHERE id = 1
-
-select substr(en_name, 1, 1) from pms_product_words_lib where id = 1
